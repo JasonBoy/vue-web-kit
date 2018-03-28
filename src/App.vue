804568ef980cc5a33de60dda215c843aecad9ce1
Jason <jasonlikenfs@gmail.com>
2018-03-28 13:33:20 +0800

    :sparkles:add JSX support

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <template>
   <div id="app">
     <img src="./logo.png">
+    <jsx-demo :name="name"></jsx-demo>
     <router-view></router-view>
   </div>
 </template>
 
-<script>
+<script type="text/jsx">
+  import Vue from 'vue';
+
+  const jsxDemo = Vue.component('jsxDemo', {
+    data() {
+      return {
+        jsxName: 'jsx'
+      }
+    },
+    props: ['name'],
+    render(h, obj) {
+      console.log('obj:', obj);
+      return (
+        <div>
+          <p>jsxName: {this.jsxName}</p>
+          <p>appName: {this.name}</p>
+        </div>
+    )
+    }
+  });
+
   export default {
     name: 'app',
+    components: {jsxDemo},
+    data() {
+      return {
+        name: 'App'
+      }
+    }
   };
 </script>
 
