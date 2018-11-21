8c5366469a34bc9a68a98edf20d145b1d82aae2f
Jason <jasonlikenfs@gmail.com>
2018-11-21 16:46:12 +0800

    :wrench:Disable ctx default respond in proxy

--- routes/proxy.js
+++ routes/proxy.js
@@ -13,7 +13,8 @@ module.exports = {
 function handleApiRequests(prefix, apiEndpoint) {
   const router = new Router({ prefix: prefix });
   router.all('*', async function(ctx) {
-    await apiProxy.dispatchRequest.call(ctx, apiEndpoint, false, { prefix });
+    ctx.respond = false;
+    apiProxy.dispatchRequest.call(ctx, apiEndpoint, false, { prefix });
   });
   return router.routes();
 }
