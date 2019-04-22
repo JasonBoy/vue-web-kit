9fc0fa80e54fc0ce576c79de919be4df8ab70477
jason <jasonlikenfs@gmail.com>
2019-04-22 22:50:04 +0800

    :nail_care:Prettify demo page logo

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,51 @@
 <template>
   <div id="app">
-    <img src="./logo.png">
+    <img src="./logo.png" />
     <jsx-demo :name="name"></jsx-demo>
     <router-view></router-view>
   </div>
 </template>
 
 <script type="text/jsx">
-  import Vue from 'vue';
+import Vue from 'vue';
 
-  const jsxDemo = Vue.component('jsxDemo', {
-    data() {
-      return {
-        jsxName: 'jsx'
-      }
-    },
-    props: ['name'],
-    render(h, obj) {
-      console.log('obj:', obj);
-      return (
-        <div>
-          <p>jsxName: {this.jsxName}</p>
-          <p>appName: {this.name}</p>
-        </div>
-    )
+const jsxDemo = Vue.component('jsxDemo', {
+  data() {
+    return {
+      jsxName: 'jsx'
     }
-  });
+  },
+  props: ['name'],
+  render(h, obj) {
+    console.log('obj:', obj);
+    return (
+      <div>
+        <p>jsxName: {this.jsxName}</p>
+        <p>appName: {this.name}</p>
+      </div>
+  )
+  }
+});
 
-  export default {
-    name: 'app',
-    components: {jsxDemo},
-    data() {
-      return {
-        name: 'App'
-      }
+export default {
+  name: 'app',
+  components: {jsxDemo},
+  data() {
+    return {
+      name: 'App'
     }
-  };
+  }
+};
 </script>
 
 <style lang="scss">
-  #app {
-    text-align: center;
-  }
+#app {
+  text-align: center;
+}
 </style>
 <style lang="scss" scoped>
-  img {
-    width: 50%;
-  }
+img {
+  width: auto;
+  height: 200px;
+}
 </style>
